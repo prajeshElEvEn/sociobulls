0c60fb60cabd0a3f53284508176c1306e4f4adc0
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-12-08 09:07:59 +0530

    updated cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const startServer = () => {
   const port = process.env.PORT || 5000;
   const app = express();
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: [process.env.CLIENT_URL],
+      methods: ["POST", "GET", "PUT", "DELETE"],
+      credentials: true,
+    })
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use("/uploads", express.static(constants.paths.uploadDir));
